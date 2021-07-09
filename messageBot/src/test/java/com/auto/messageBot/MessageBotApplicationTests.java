a158dcb38b88278592c279da1b245f0f293d7092
autotradegeneral <87162206+autotradegeneral@users.noreply.github.com>
2021-07-09 23:24:44 +0800

    Add files via upload

---
+++ messageBot/src/test/java/com/auto/messageBot/MessageBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.messageBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
